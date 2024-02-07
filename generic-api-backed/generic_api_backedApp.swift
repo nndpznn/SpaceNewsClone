1a0e290123acde63e6a26d0e1633c437f14edd52
Nolan Nguyen! <nolan2087@gmail.com>
2024-02-07 12:32:25 -0800

    Initial Commit

---
+++ generic-api-backed/generic_api_backedApp.swift
@@ -0,0 +1,17 @@
+//
+//  generic_api_backedApp.swift
+//  generic-api-backed
+//
+//  Created by Nolan Nguyen on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct generic_api_backedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ generic-api-backedTests/generic_api_backedTests.swift
@@ -0,0 +1,36 @@
+//
+//  generic_api_backedTests.swift
+//  generic-api-backedTests
+//
+//  Created by Nolan Nguyen on 2/7/24.
+//
+
+import XCTest
+@testable import generic_api_backed
+
+final class generic_api_backedTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
