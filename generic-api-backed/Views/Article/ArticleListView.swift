6d0cc5aedfde6e7be7913f05c938bd822e5563b1
Nolan Nguyen! <nolan2087@gmail.com>
2024-02-21 13:09:11 -0800

    attemping to add loading view. having trouble, but basic functionality is still there

--- generic-api-backed/Views/Article/ArticleListView.swift
+++ generic-api-backed/Views/Article/ArticleListView.swift
@@ -16,76 +16,74 @@ struct ArticleListView: View {
     @State var appearing: Bool = false
     
     var body: some View {
-
-        NavigationStack {
-            ZStack {
-                ForEach(0..<200) { _ in // Adjust the number of stars as needed
-                    StarView()
-                }
-                
-                VStack(alignment: .leading) {
-                    HStack {
-                        
-                        Image(systemName: "moon.stars.fill")
-                            .foregroundStyle(.white)
-                        
-                        Text("Trending Articles")
-                            .font(.title)
-                            .foregroundStyle(.white)
-                            .bold()
-                        
-                        Spacer()
-                        
-                        SearchButton(isSet: $notSearching)
+        if loading {
+            ProgressView()
+        } else {
+            NavigationStack {
+                ZStack {
+                    ForEach(0..<200) { _ in // Adjust the number of stars as needed
+                        StarView()
                     }
-                    if articles.count > 0 {
-                        ScrollView {
-                            ForEach(articles) { article in
-                                NavigationLink {
-                                    ArticleDetailView(article: article)
-                                } label: {
-                                    ArticleItemView(article: article)
-                                }
-                            }
-                        }
-                        .animation(Animation.bouncy(duration: 1.0), value:articles)
-//                        .animation(Animation.bouncy(duration: 1.0), value: appearing)
-//                        .onAppear() {
-//                            appearing = true
-//                        }
-                        
-                        .navigationBarTitle("Trending Articles", displayMode: .inline).navigationBarHidden(notSearching)
-                    } else {
-                        Divider()
-                        VStack(alignment: .center) {
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
                             
-                            Text("Sent our satellites out... couldn't find a thing.")
+                            Image(systemName: "moon.stars.fill")
                                 .foregroundStyle(.white)
-                                .font(.headline)
+                            
+                            Text("Trending Articles")
+                                .font(.title)
+                                .foregroundStyle(.white)
+                                .bold()
                             
                             Spacer()
                             
-                            Text("Maybe your answer is in these stars?")
-                                .foregroundStyle(.white)
-                                .font(.headline)
+                            SearchButton(isSet: $notSearching)
                         }
+                        if articles.count > 0 {
+                            ScrollView {
+                                ForEach(articles) { article in
+                                    NavigationLink {
+                                        ArticleDetailView(article: article)
+                                    } label: {
+                                        ArticleItemView(article: article)
+                                    }
+                                }
+                            }
+                            .animation(Animation.bouncy(duration: 1.0), value:articles)
+                            
+                            .navigationBarTitle("Trending Articles", displayMode: .inline).navigationBarHidden(notSearching)
+                        } else {
+                            Divider()
+                            VStack(alignment: .center) {
+                                
+                                Text("Sent our satellites out... couldn't find a thing.")
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                
+                                Spacer()
+                                
+                                Text("Maybe your answer is in these stars?")
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                            }
+                        }
+                    } .task {
+                        await loadSearchArticles(text: searchText)
                     }
-                } .task {
+                    .padding()
+                }
+                .background(.black)
+            }
+            .searchable(text: $searchText, prompt: "Search Articles")
+            .onChange(of: searchText) {
+                Task {
                     await loadSearchArticles(text: searchText)
                 }
-                .padding()
             }
-            .background(.black)
+            .navigationViewStyle(StackNavigationViewStyle())
         }
-        .searchable(text: $searchText, prompt: "Search Articles")
-        .onChange(of: searchText) {
-            Task {
-                await loadSearchArticles(text: searchText)
-            }
-        }
-        .navigationViewStyle(StackNavigationViewStyle())
     }
-    
     
     func loadArticles() async {
         errorOccurred = false
@@ -93,7 +91,6 @@ struct ArticleListView: View {
         
         do {
             let loadedArticles = try await getArticles()
-            loading = false
             articles = loadedArticles.results
         } catch {
             errorOccurred = true
@@ -106,8 +103,8 @@ struct ArticleListView: View {
         loading = true
         
         do {
-            let loadedArticles = try await getArticlesSearch(query: text )
             loading = false
+            let loadedArticles = try await getArticlesSearch(query: text )
             articles = loadedArticles.results
         } catch {
             errorOccurred = true
--- generic-api-backed/Views/Report/ReportListView.swift
+++ generic-api-backed/Views/Report/ReportListView.swift
@@ -16,76 +16,74 @@ struct ReportListView: View {
     @State var appearing: Bool = false
     
     var body: some View {
-
-        NavigationStack {
-            ZStack {
-                ForEach(0..<200) { _ in // Adjust the number of stars as needed
-                    StarView()
-                }
-                
-                VStack(alignment: .leading) {
-                    HStack {
-                        
-                        Image(systemName: "moon.stars.fill")
-                            .foregroundStyle(.white)
-                        
-                        Text("Latest Reports")
-                            .font(.title)
-                            .foregroundStyle(.white)
-                            .bold()
-                        
-                        Spacer()
-                        
-                        SearchButton(isSet: $notSearching)
+        if loading {
+            ProgressView()
+        } else {
+            NavigationStack {
+                ZStack {
+                    ForEach(0..<200) { _ in // Adjust the number of stars as needed
+                        StarView()
                     }
-                    if reports.count > 0 {
-                        ScrollView {
-                            ForEach(reports) { report in
-                                NavigationLink {
-                                    ReportDetailView(report: report)
-                                } label: {
-                                    ReportItemView(report: report)
-                                }
-                            }
-                        }
-                        .animation(Animation.bouncy(duration: 1.0), value: reports)
-//                        .animation(Animation.bouncy(duration: 1.0), value: appearing)
-//                        .onAppear() {
-//                            appearing = true
-//                        }
-                        
-                        .navigationBarTitle("Latest Reports", displayMode: .inline).navigationBarHidden(notSearching)
-                    } else {
-                        Divider()
-                        VStack(alignment: .center) {
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
                             
-                            Text("Sent our satellites out... couldn't find a thing.")
+                            Image(systemName: "moon.stars.fill")
                                 .foregroundStyle(.white)
-                                .font(.headline)
+                            
+                            Text("Latest Reports")
+                                .font(.title)
+                                .foregroundStyle(.white)
+                                .bold()
                             
                             Spacer()
                             
-                            Text("Maybe your answer is in these stars?")
-                                .foregroundStyle(.white)
-                                .font(.headline)
+                            SearchButton(isSet: $notSearching)
                         }
+                        if reports.count > 0 {
+                            ScrollView {
+                                ForEach(reports) { report in
+                                    NavigationLink {
+                                        ReportDetailView(report: report)
+                                    } label: {
+                                        ReportItemView(report: report)
+                                    }
+                                }
+                            }
+                            .animation(Animation.bouncy(duration: 1.0), value: reports)
+                            
+                            .navigationBarTitle("Latest Reports", displayMode: .inline).navigationBarHidden(notSearching)
+                        } else {
+                            Divider()
+                            VStack(alignment: .center) {
+                                
+                                Text("Sent our satellites out... couldn't find a thing.")
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                
+                                Spacer()
+                                
+                                Text("Maybe your answer is in these stars?")
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                            }
+                        }
+                    } .task {
+                        await loadSearchReports(text: searchText)
                     }
-                } .task {
+                    .padding()
+                }
+                .background(.black)
+            }
+            .searchable(text: $searchText, prompt: "Search Reports")
+            .onChange(of: searchText) {
+                Task {
                     await loadSearchReports(text: searchText)
                 }
-                .padding()
             }
-            .background(.black)
+            .navigationViewStyle(StackNavigationViewStyle())
         }
-        .searchable(text: $searchText, prompt: "Search Reports")
-        .onChange(of: searchText) {
-            Task {
-                await loadSearchReports(text: searchText)
-            }
-        }
-        .navigationViewStyle(StackNavigationViewStyle())
     }
-
     
     func loadReports() async {
         errorOccurred = false
@@ -93,7 +91,6 @@ struct ReportListView: View {
         
         do {
             let loadedReports = try await getReports()
-            loading = false
             reports = loadedReports.results
         } catch {
             errorOccurred = true
@@ -106,8 +103,8 @@ struct ReportListView: View {
         loading = true
         
         do {
-            let loadedReports = try await getReportsSearch(query: text)
             loading = false
+            let loadedReports = try await getReportsSearch(query: text)
             reports = loadedReports.results
         } catch {
             errorOccurred = true
@@ -115,7 +112,6 @@ struct ReportListView: View {
         loading = false
     }
 }
-
 
 #Preview {
     ReportListView()
